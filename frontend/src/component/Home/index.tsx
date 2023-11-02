27567ffaefc6ad929700ed9b5af69af1e4efc5e4
Nadir Hossain <nadirhossain336@gmail.com>
2023-11-03 00:58:16 +0600

    implement navbar

---
+++ frontend/src/component/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Navbar/>
+      <div></div>
+    </>
+  )
+}
+
+export default Home
---
+++ frontend/src/component/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Navbar.scss"
+import { FaFacebookMessenger } from 'react-icons/fa';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className='max-w-7xl mx-auto px-10 navbar'>
+      <div className='navbar-container'>
+        <div className='desktop-brand-name'>Chat</div>
+        <div className='mobile-brand-name'>CC</div>
+        <div className='right-container'>
+          <FaFacebookMessenger size={20}/>
+          <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
